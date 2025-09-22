84c09453783edd592bda7ea90546e949abe604f8
tpchuckles <tpchuckles@gmail.com>
2025-09-22 13:45:06 -0400

    had extra negative for defocus. simplify by allowing sign of dz to control multiply or division. add 3D visualization of convergent probe to 07_defocus test

--- src/multislice/multislice.py
+++ src/multislice/multislice.py
@@ -184,10 +184,10 @@ class Probe:
         kx_grid, ky_grid = xp.meshgrid(self.kxs, self.kys, indexing='ij')
         k_squared = kx_grid**2 + ky_grid**2
         P = xp.exp(-1j * xp.pi * self.wavelength * dz * k_squared)
-        if dz>0:
-            self.array = xp.fft.ifft2( P * xp.fft.fft2( self.array ) )
-        if dz<0:
-            self.array = xp.fft.ifft2( xp.fft.fft2( self.array ) / P )
+        #if dz>0:
+        self.array = xp.fft.ifft2( P * xp.fft.fft2( self.array ) )
+        #if dz<0:
+        #   self.array = xp.fft.ifft2( xp.fft.fft2( self.array ) / P )
 
 
 def probe_grid(xlims,ylims,n,m):
