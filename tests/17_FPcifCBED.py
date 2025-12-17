a69a1cd71ee8e8bc34ca0ed4f98716d97a6e6d77
tpchuckles <tpchuckles@gmail.com>
2025-12-17 11:18:33 -0500

    add seed to 17_... test. FP needs seed to not regen every time

--- tests/17_FPcifCBED.py
+++ tests/17_FPcifCBED.py
@@ -3,25 +3,22 @@ try:
     import pyslice
 except ModuleNotFoundError:
     sys.path.insert(0, '../src')
-from pyslice.io.loader import Loader
-from pyslice.multislice.potentials import gridFromTrajectory, Potential
-from pyslice.multislice.multislice import probe_grid
-from pyslice.postprocessing.haadf_data import HAADFData
-from pyslice.postprocessing.testtools import differ
-from pyslice.multislice.calculators import MultisliceCalculator
+
+from pyslice import Loader,probe_grid,gridFromTrajectory,Potential,MultisliceCalculator,HAADFData,differ
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 cif = "inputs/hBN.cif" ; a,b=2.4907733333333337,2.1570729817355123
 trajectory=Loader(cif).load()
 trajectory = trajectory.tile_positions([5,5,1])
-trajectory = trajectory.generate_random_displacements(n_displacements=20,sigma=.1)
+trajectory = trajectory.generate_random_displacements(n_displacements=20,sigma=.1,seed=0) # PRO TIP: use a random seed so you get repeatable "random" configurations and don't need to re-multislice on subsequent runs
 
 positions = trajectory.positions[0]
 atom_types=trajectory.atom_types
 xs,ys,zs,lx,ly,lz=gridFromTrajectory(trajectory,sampling=0.1,slice_thickness=0.5)
 potential = Potential(xs, ys, zs, positions, atom_types, kind="kirkland")
-potential.plot()
+potential.plot("outputs/figs/17_potential.png")
 
 xy=probe_grid([a,3*a],[b,3*b],14,16) # comment out to default to middle position
 #xy = None
@@ -30,11 +27,11 @@ calculator=MultisliceCalculator()
 calculator.setup(trajectory,aperture=30,voltage_eV=100e3,sampling=.1,slice_thickness=.5,probe_positions=xy)
 exitwaves = calculator.run()
 
-exitwaves.plot_reciprocal() # can also specify whichProbe and whichTimestep instead of averaging
+exitwaves.plot_reciprocal("outputs/figs/17_cbed.png") # can also specify whichProbe and whichTimestep instead of averaging
 
 haadf=HAADFData(exitwaves)
 ary=haadf.calculateADF(preview=False) # use preview=True to view the collection angles of the ADF detector in reciprocal space
-haadf.plot()
+haadf.plot("outputs/figs/17_haadf.png")
 
 
 array = exitwaves.array
